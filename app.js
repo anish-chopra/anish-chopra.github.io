15c4e009050f2677fba18935a421b58b3809c57d
“Anish <chopra.anish@gmail.com>
2023-05-15 18:02:46 -0400

    temp add 2nd javascript event

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const treehouse = document.getElementById('treehouse');
 const codecademy = document.getElementById('codecademy');
 const fewd1 = document.getElementById('fewd');
 const code1 = document.getElementById('code');
+const toggleButton = document.getElementById('mode')
 function fewd() {
     if (treehouse.style.display == "none") {
         treehouse.style.width = "100%"
@@ -20,3 +21,18 @@ function code() {
 }
 fewd1.addEventListener("click", fewd);
 code1.addEventListener("click", code);
+
+// function nightMode() {
+//     if (document.body.style.fontFamily == "arial")
+//     document.body.style.fontFamily = "comic sans"
+//     else {
+//         document.body.style.fontFamily = "arial"
+//     }
+// }
+
+// mode.addEventListener("click", nightMode);
+function change() {
+    let getColor = document.querySelector('.arial');
+    getColor.classList.toggle('weirdFont');
+}
+toggleButton.addEventListener('click', change)
